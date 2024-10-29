bef81e3915808f57f58e681543c5ef62f11e36a7
권서영 <kwonseoy@gmail.com>
2024-10-29 09:54:17 +0900

    usage of alarm and signal added

--- mining/ssafy.c
+++ mining/ssafy.c
@@ -5,7 +5,20 @@
 #include <unistd.h>
 #include <string.h>
 
-void send_signal(pid_t mine_pid, int signal)
+// Global variable to store mineApp PID
+pid_t mine_pid;
+
+// Signal handler for alarm (timeout)
+void handle_alarm(int sig)
+{
+    printf("No input received in 5 seconds. Exiting...\n");
+    // Send SIGTERM to mineApp to terminate it
+    kill(mine_pid, SIGTERM);
+    exit(0); // Exit ssafyApp
+}
+
+// Function to send a signal to mineApp
+void send_signal(int signal)
 {
     if (kill(mine_pid, signal) == -1)
     {
@@ -22,32 +35,46 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    pid_t mine_pid = atoi(argv[1]);
+    mine_pid = atoi(argv[1]);
     char command[10];
 
     printf("ssafyApp started. PID: %d\n", getpid());
 
+    // Set up the alarm signal handler
+    struct sigaction sa;
+    sa.sa_handler = handle_alarm;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGALRM, &sa, NULL);
+
     while (1)
     {
+        // Start a 5-second alarm
+        alarm(5);
+
         printf("Enter command (SHOW, ADD, STOP, exit): ");
         if (fgets(command, sizeof(command), stdin) == NULL)
         {
             break;
         }
 
-        command[strcspn(command, "\n")] = '\0'; // Remove newline character
+        // Reset the alarm since we received input
+        alarm(0);
+
+        // Remove newline character from command
+        command[strcspn(command, "\n")] = '\0';
 
         if (strcmp(command, "SHOW") == 0)
         {
-            send_signal(mine_pid, SIGUSR1);
+            send_signal(SIGUSR1);
         }
         else if (strcmp(command, "ADD") == 0)
         {
-            send_signal(mine_pid, SIGUSR2);
+            send_signal(SIGUSR2);
         }
         else if (strcmp(command, "STOP") == 0 || strcmp(command, "exit") == 0)
         {
-            send_signal(mine_pid, SIGTERM);
+            send_signal(SIGTERM);
             if (strcmp(command, "exit") == 0)
             {
                 break;
